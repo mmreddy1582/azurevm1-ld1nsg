e9d72f0dc921459b0e901859b0ea73bff2cc6a52
Muthirevula, Muragaiah <Muragaiah.Muthirevula@aia.com>
2020-10-27 00:05:28 +0800

    updated remote state

---
+++ terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "vmssremotepoc-rg1"
+        storage_account_name = "vmsstfremotestorage"
+        container_name       = "vmtfstate"
+        key                  = "vmstate.tfstate"
+    }
+}
